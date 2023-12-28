3816c8d596b8701be42da95a49495ac861896b50
raulhubgit <89980156+raulhubgit@users.noreply.github.com>
2023-12-28 19:57:01 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,19 @@ import firebase from 'firebase';
 
 // adicione SDK do Firebase
 var firebaseConfig = {
+    apiKey: "AIzaSyD1GQiaov2jaDLA4YvYCvFvm40YDVcP8TI",
 
+    authDomain: "pj65-ae596.firebaseapp.com",
+  
+    databaseURL: "https://pj65-ae596-default-rtdb.europe-west1.firebasedatabase.app",
+  
+    projectId: "pj65-ae596",
+  
+    storageBucket: "pj65-ae596.appspot.com",
+  
+    messagingSenderId: "675431023638",
+  
+    appId: "1:675431023638:web:26a4e21f1b40a43484a137" 
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
